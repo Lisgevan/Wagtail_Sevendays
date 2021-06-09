5130e3eb9f74159d97d59847c82d86a2636c4ff8
Lisgevan <episimo@hotmail.com>
2021-06-09 10:49:31 +0300

    Days 2-3-4-5

---
+++ generic/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+
+class GenericPage(Page):
+    banner_title = models.CharField(
+        max_length=100, 
+        default='Welcome to my generic page!'
+    )
+    introduction = models.TextField(blank=True)
+    banner_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    author = models.ForeignKey(
+        'Author',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title"),
+        FieldPanel("introduction"),
+        ImageChooserPanel("banner_image"),
+        SnippetChooserPanel("author",)
+    ]
+
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(blank=True, max_length=100)
+    company_name = models.CharField(blank=True, max_length=100)
+    company_url = models.URLField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='+'
+    )
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("title"),
+        FieldPanel("company_name"),
+        FieldPanel("company_url"),
+        ImageChooserPanel("image"),
+    ]
+
+    def __str__(self) -> str:
+        return self.name
