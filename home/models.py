61c80d7aa01617dbb5ffd196404e47db221ec6c1
Lisgevan <episimo@hotmail.com>
2021-06-14 18:54:48 +0300

    Carousel added

--- home/models.py
+++ home/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.blocks import ImageChooserBlock
 
-
+class Carousel(blocks.StructBlock):
+    image = ImageChooserBlock()
+    text = blocks.RichTextBlock()
 class HomePage(Page):
     banner_title = models.CharField(max_length=100, default='Welcome to my Homepage!')
 
+    body = StreamField([
+        #('name', blocks.SomethingBlock()),
+        ('heading', blocks.CharBlock(template='heading_block.html')),
+        ('image', ImageChooserBlock()),
+        ('paragraph', blocks.RichTextBlock()),
+        ('carousel', Carousel()),
+    ], null=True)
+
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
+        StreamFieldPanel("body"),
     ]
