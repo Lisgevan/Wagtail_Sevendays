08f605e5092dca0a78d0327552c59fd58f97e7b0
Lisgevan <episimo@hotmail.com>
2021-06-15 20:01:30 +0300

    added footer content

--- site_settings/models.py
+++ site_settings/models.py
@@ -5,8 +5,18 @@ from wagtail.contrib.settings.models import BaseSetting, register_setting
 
 @register_setting
 class SocialMediaSettings(BaseSetting):
+    facebook = models.URLField(max_length=100, blank=True)
     instagram = models.URLField(max_length=100, blank=True)
+    twitter = models.URLField(max_length=100, blank=True)
+    pinterest = models.URLField(max_length=100, blank=True)
+    youtube = models.URLField(max_length=100, blank=True)
+    github = models.URLField(max_length=100, blank=True)
 
     panels = [
-        FieldPanel("instagram")
+        FieldPanel("facebook"),
+        FieldPanel("instagram"),
+        FieldPanel("twitter"),
+        FieldPanel("pinterest"),
+        FieldPanel("youtube"),
+        FieldPanel("github"),
     ]
